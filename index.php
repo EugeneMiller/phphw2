ca6efcc6ada82c8e7c6f8dfa2b30f05a335951dc
Евгений Мельник <good.production@rambler.ru>
2022-08-19 00:24:15 +0500

    first commit

---
+++ index.php
@@ -0,0 +1,26 @@
+<?php
+class Point
+{
+    private $x;
+    private $y;
+    function __construct($x, $y)
+    {
+        $this->x = $x;
+        $this->y = $y;
+    }
+    public function __toString()
+    {
+        return "(" . $this->x . ":" . $this->y . ")";
+    }
+    public function __get($index)
+    {
+        //echo "!".$index."!";
+        if ($index == "xCoord") {
+            echo "!".$this->x."!";
+        }
+    }
+}
+$p = new Point(100, 200);
+//echo $p . '<br/>';
+echo "[".$p->xCoord."]";
+print_r(PDO::getAvailableDrivers());
